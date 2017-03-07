3e402dba4257658eeab5e1604b8baf46debddf93
Raphael COUSIN <racousin@e1r5p6.42.fr>
2017-03-07 12:48:23 +0100

    available to put lights and move light (one, cause of opencl), hierarchy between lights and objects doesn't work

--- srcs/converter/gtk2cl.c
+++ srcs/converter/gtk2cl.c
@@ -7,12 +7,38 @@
 #include "ui.h"
 #include "renderer.h"
 
-int		gtk_objects2nb_obj(t_list *objects)
+
+
+//TODO CHOOSE LIGHT OR SPOT
+int		is_obj(t_list	*objects)
 {
-	//TODO ne pas compter les obj empty
-	if(!objects)
-		return (0);
-	return (1 + gtk_objects2nb_obj(objects->next) + gtk_objects2nb_obj(objects->children));
+	int	type;
+
+	type = ((t_object*)(objects->content))->type;
+	return (type < 4);
+}
+
+int		is_light(t_list	*objects)
+{
+	int	type;
+
+	type = ((t_object*)(objects->content))->type;
+	return (type == 5);
+}
+
+void	gtk_objects2nb_obj(t_list *objects, int	*nb_obj, int *nb_light)
+{
+	int	type;
+
+	if (!objects)
+		return;
+	type = ((t_object*)(objects->content))->type;
+	if (type < 4)
+		(*nb_obj)++;
+	else if (type == 5)
+		(*nb_light)++;
+	gtk_objects2nb_obj(objects->next, nb_obj, nb_light);
+	gtk_objects2nb_obj(objects->children, nb_obj, nb_light);
 }
 
 void	vect3d_2_float3(FLOAT3 *vect_cl, t_vector3d vect_c)
@@ -31,7 +57,7 @@ FLOAT3	vector3d_to_float3(t_vector3d v)
 	v2.z = v.z;
 	return (v2);
 }
-//pur vector3d flot3 operato in other file
+//TODO put vector3d flot3 operator in other file
 t_vector3d	less(t_vector3d a, t_vector3d b)
 {
 	a.x -= b.x;
@@ -67,18 +93,28 @@ void	obj_c_2_cl(t_obj *obj, t_list *objs, t_obj *parent)
 	if (parent)
 		((*obj).pos) = add_cl((*obj).pos, (*parent).pos);
 }
+void	spot_c_2_cl(t_spot *spot, t_list *objs, t_spot *parent)
+{
+	t_object	objects;
+	t_object	objects_parent;
+
+	objects = *((t_object*)(objs->content));
+	vect3d_2_float3(&((*spot).pos), objects.pos);
+	if (parent)
+		((*spot).pos) = add_cl((*spot).pos, (*parent).pos);
+}
+		//need to optimise this shit !
 void	fill_obj(t_list *objects, t_obj *obj, int *id, int	*parent)
 {
 	int	tmp;
 
 	tmp = *id;
-	if (objects)
+	if (objects && is_obj(objects))
 	{
 		if (*parent != -1)
 			obj_c_2_cl(&(obj[*id]), objects, &(obj[*parent]));
 		else
 			obj_c_2_cl(&(obj[*id]), objects, 0);
-		//need to optimise this triple if !
 		obj[*id].param = 1;
 		obj[*id].type = SPHERE; // ICI OMG
 		obj[*id].id = *id;
@@ -88,46 +124,87 @@ void	fill_obj(t_list *objects, t_obj *obj, int *id, int	*parent)
 	}
 	if (objects->next)
 	{
-		(*id)++;
+		if(is_obj(objects))
+			(*id)++;
 		fill_obj(objects->next, obj, id, parent);
 	}
 	if (objects->children)
 	{
 		*parent = tmp;
-		(*id)++;
+		if(is_obj(objects))
+			(*id)++;
 		fill_obj(objects->children, obj, id, parent);
 	}
 	//TODO for children is necessary to ajust the value with scale
+}
+//TODO for now, just one spot is effective and hierarchy spot obj doesn't work
+void	fill_spot(t_list *objects, t_spot *spot, int *id, int *parent)
+{
+	int	tmp;
+
+	tmp = *id;
+	if (objects && is_light(objects))
+	{
+		if (*parent != -1)
+			spot_c_2_cl(&(spot[*id]), objects, &(spot[*parent]));
+		else
+			spot_c_2_cl(&(spot[*id]), objects, 0);
+		spot[*id].color.x = 1;
+		spot[*id].color.y = 1;
+		spot[*id].color.z = 0;
+		spot[*id].intensity = 1;
+		//spot[*id].id = *id; besoin d'un id?
+	}
+	if (objects->next)
+	{
+		if(is_light(objects))
+			(*id)++;
+		fill_spot(objects->next, spot, id, parent);
+	}
+	if (objects->children)
+	{
+		*parent = tmp;
+		if(is_light(objects))
+			(*id)++;
+		fill_spot(objects->children, spot, id, parent);
+	}
 }
 
 void	ask_for_new_image(t_ui *ui)
 {
 	t_obj	*obj;
-	t_spot	spot;
+	t_spot	*spot;
 	int		nb_obj;
+	int		nb_light;
 	int		id;
 	int		parent;
 
-	//sera modifier dynamiquement par la suite
-	spot.color.x = 1;
-	spot.color.y = 1;
-	spot.color.z = 1;
-	spot.intensity = 1;
-	spot.pos.x = 0;
-	spot.pos.y = 1;
-	spot.pos.z = 7;
-	spot.color.x = 1;
-	spot.color.y = 1;
-	spot.color.z = 1;
-	spot.intensity = 1;
 
-	nb_obj = gtk_objects2nb_obj(ui->objs);
+	nb_obj = 0;
+	nb_light = 0;
+	gtk_objects2nb_obj(ui->objs, &nb_obj, &nb_light);
+
 	obj = (t_obj*)malloc(sizeof(t_obj) * nb_obj);
-	if(!obj)
+	spot = (t_spot*)malloc(sizeof(t_spot) * (nb_light + 1));
+	if(!obj || !spot)
 		exit(EXIT_FAILURE);
+
 	id = 0;
 	parent = -1;
 	fill_obj(ui->objs, obj, &id, &parent);
+	id = 0;
+	parent = -1;
+	spot[0].color.x = 1;
+	spot[0].color.y = 1;
+	spot[0].color.z = 1;
+	spot[0].intensity = 1;
+	spot[0].pos.x = 0;
+	spot[0].pos.y = 1;
+	spot[0].pos.z = 7;
+
+	fill_spot(ui->objs, spot, &id, &parent);
+
+	//sera modifier dynamiquement par la suite
 	ui->scene.nb_obj = nb_obj;
 	//ui->scene.cam = *(ui->cam);
 	ui->scene.cam.dir = vector3d_to_float3(ui->cam->dir);
@@ -145,6 +222,7 @@ void	ask_for_new_image(t_ui *ui)
 	ui->scene.cam.ratio = 1.0;
 	ui->scene.nb_spot = 1;
 	ui->scene.obj = obj;
-	ui->scene.light = &spot;
+	ui->scene.light = spot;
+
 	ui_print_scene(renderer_compute_image((&(ui->scene))));
 }
