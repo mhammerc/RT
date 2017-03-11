ff4d815962022323a2697eddc56b141246a40c1c
Raphael_Cousin <racousin@student.42.fr>
2017-03-11 10:50:02 +0100

    union

--- srcs/renderer/objects/csg.c
+++ srcs/renderer/objects/csg.c
@@ -87,7 +87,7 @@ void	inter(t_interval *left, t_interval *right, t_interval *interval)
 }
 
 /*
-** particulary case solve quickly in intersecion interval
+** particulary case solve quickly in substraction interval
 **
 */
 
@@ -231,8 +231,125 @@ void	minus(t_interval *left, t_interval *right, t_interval *interval)
 }
 
 /*
+** particulary case solve quickly in union interval
+**
+*/
+
+int	union_case(t_interval *left, t_interval *right, t_interval *interval)
+{
+	if (left->nb_hit == 0 && right->nb_hit == 0)
+	{
+		interval->nb_hit = 0;
+		return (1);
+	}
+	if (left->nb_hit == 0)
+	{
+		*interval = *right;
+		return (1);
+	}
+	if (right->nb_hit == 0)
+	{
+		*interval = *left;
+		return (1);
+	}
+	return (0);
+}
+/*
+**
+**test overlap //NOTABENE cette fonction nest pas utilise
+*/
+
+int	test_union_overlap(t_interval *a, int i, int j)
+{
+	double	min1;
+	double	min2;
+	double	max1;
+	double	max2;
+	
+	min1 = a->min[i].dist;
+	max1 = a->max[i].dist;
+	min2 = a->min[j].dist;
+	max2 = a->max[j].dist;
+	if (min1 < min2 && max1 < min2)
+		return (0);
+	if (min2 < min1 && max2 < min1)
+		return (0);
+	return (1);
+}
+
+/*
+**
+**modify : union between a_i and a_j put in a_i
+*/
+void	modify_by_union(t_interval *a_i, t_interval *a_j, int i, int j)
+{
+	if (a_j->min[j].dist < a_i->min[i].dist)
+		a_i->min[i] = a_j->min[j];
+	if (a_j->max[j].dist > a_i->max[i].dist)
+		a_i->max[i] = a_j->max[j];
+}
+
+/*
+**
+**result in interval : left union right
+*/
+void	ft_union(t_interval *left, t_interval *right, t_interval *interval)
+{
+	int	i;
+	int	j;
+	int	test[10];
+
+	if (union_case(left, right, interval))
+		return;
+	i = 0;
+	while (i < right->nb_hit)
+	{
+		left->min[i + left->nb_hit] = right->min[i];
+		left->max[i + left->nb_hit] = right->max[i];
+		test[i] = 1;
+		i++;
+	}
+	while (i < left->nb_hit + right->nb_hit)
+		test[i++] = 1;
+	while (i < 10)
+		test[i++] = 0;
+	i = 0;
+	while (i < 9)
+	{
+		j = i + 1;
+		while (j < 10)
+		{
+			if (test[j])
+			{
+				if (!is_disjoint(left, left, i, j))
+				{
+					modify_by_union(left, left, i, j);
+					test[j] = 0;
+					j = i;
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+	j = 0;
+	while (j < 10)
+	{
+		if (test[j])
+		{
+			interval->min[i] = left->min[j];
+			interval->max[i] = left->max[j];
+			i++;
+		}
+		j++;
+	}
+	interval->nb_hit = i;
+}
+
+/*
 **recursion fonction which run csg tree_obj, and return in intervall, segments cross by ray
-**and the ref of the normal for each of them
+**and the ref of the object for each of them
 */
 
 void			test_csg(t_obj *obj, t_ray *ray, t_interval *interval)
@@ -263,7 +380,7 @@ void			test_csg(t_obj *obj, t_ray *ray, t_interval *interval)
 		test_csg(obj->left, ray, &left);
 		test_csg(obj->right, ray, &right);
 		if (obj->csg == 'U')
-			inter(&left, &right, interval);
+			ft_union(&left, &right, interval);
 		else if (obj->csg == 'I')
 			inter(&left, &right, interval);
 		else if (obj->csg == '-')
