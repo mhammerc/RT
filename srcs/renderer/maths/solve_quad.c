2734cfdf38656dc426dccfdc0d7d9429a31cbed0
Raphael COUSIN <racousin@estudent.42.fr>
2017-03-18 19:50:24 +0100

    torus works half

--- srcs/renderer/maths/solve_quad.c
+++ srcs/renderer/maths/solve_quad.c
@@ -6,7 +6,7 @@
 /*   By: racousin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/14 11:09:33 by racousin          #+#    #+#             */
-/*   Updated: 2017/03/18 18:14:37 by racousin         ###   ########.fr       */
+/*   Updated: 2017/03/18 19:49:19 by racousin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,14 +73,6 @@ double			case2(double g, double h, double a, double b)
 	double	j = pow(i, 1./3.);
 	double	K = acos(-g /(2. * i));
 		return(2* j * cos(K / 3) - b / ( 3 *a));
-//	double	L = -j;
-//	double	M = cos(K / 3.);
-//	double	N = 1.73205 * sin(K / 3);
-//	double	P = -b / (3 * a);
-//	if (root_number == 1)
-//		return (L * (M + N) + P);
-//	else
-//		return (L * (M - N) + P);
 }
 double			case3(double g, double h, double a, double b)
 {
@@ -94,9 +86,11 @@ double			quad3_solve(double a, double b, double c, double d)
 {
 	double	f = (3. * c / a - pow(b, 2.) / pow(a, 2.)) / 3;
 	double	g = (2. * pow(b, 3.) / pow(a, 3.) - 9. * b * c / pow(a, 2.) + 27 * d / a) / 27;
-	double	h = pow(g, 2) / 4 + pow(f, 3.) / 27;
-	if ( h > 0)
+	double	h = pow(g, 2.) / 4. + pow(f, 3.) / 27.;
+	if ( h > 0.)
 		return (case1(g, h, a, b));
+	else if (f < EPS && g < EPS && f > EPS && g > EPS)
+		return(pow(d / a, 1. / 3.));
 	else
 	{
 		return (case2(g, h, a, b));
@@ -105,44 +99,90 @@ double			quad3_solve(double a, double b, double c, double d)
 int		quad4_solve(double a, double b, double c, double d, t_interval *interval)
 {
 	double y = quad3_solve(1, -b,a * c - 4 *d,-pow(a,2) * d - pow(c,2) + 4 * b * d);
-	double	eps = 0;
 	double	r_g[2];
 	double	r_h[2];
-	while (!(quad2_solve(-a, b - y - eps, r_g)))
-	{
-		//printf("not g\n");
-		//r_g[0] = 0;
-		//r_g[1] = 0;
-		eps += b - y;
-	}
-	eps = 0;
-	while (!(quad2_solve(-y, d - eps, r_h)))
-	{
-		//r_h[0] = 0;
-		//r_h[1] = 0;
-		eps += d;
-		//printf("not h\n");
-	}
+	if(!(quad2_solve(-a, b - y, r_g)))
+		return (0);
+	if(!(quad2_solve(-y, d, r_h)))
+		return (0);
 	if (fabs(r_g[0] * r_h[1] + r_g[1] * r_h[0] - c) > fabs(r_g[0] * r_h[0] + r_g[1] * r_h[1] - c))	
 		{
 			double	tmp = r_h[0];
 			r_h[0] = r_h[1];
 			r_h[1] = tmp;
 		}
-	//printf("g equation: %fx2 + %fx +%f\n",1., -a, b-y);
-	//printf("h equation: %fx2 + %fx +%f\n",1., -y, d);
-	//printf("g1 %f g2 %f h1 %f h2 %f\n", r_g[0], r_g[1], r_h[0], r_h[1]);
-	//printf("result equation: x4 +%fx3 +  %fx2 + %fx +%f\n", r_g[0] + r_g[1], r_g[0] * r_g[1] + r_h[0] + r_h[1], r_g[0] * r_h[1] + r_g[1] *r_h[0], r_h[0] *r_h[1]);
 	double	root_1[2];
 	double	root_2[2];
-	bzero(root_1, sizeof(double) * 2);
-	bzero(root_2, sizeof(double) * 2);
-	if (!(quad2_solve(r_g[0], r_h[0], root_1)))
-;
-		//printf("not 1\n");
-	if (!(quad2_solve(r_g[1], r_h[1], root_2)))
-;
-		//printf("not 2\n");
+	interval->nb_hit = 0;
+	if (quad2_solve(r_g[0], r_h[0], root_1))
+	{
+		if (quad2_solve(r_g[1], r_h[1], root_2))
+		{
+			interval->nb_hit = 2;
+			if (root_1[0] < root_2[0])
+			{
+				interval->min[0].dist = root_1[0];
+				interval->min[0].normal = OUTWARDS;
+				interval->max[0].normal = INWARDS;
+				interval->min[1].normal = OUTWARDS;
+				interval->max[1].normal = INWARDS;
+				if (root_1[1] < root_2[0])
+				{
+					interval->max[0].dist = root_1[1];
+					interval->min[1].dist = root_2[0];
+					interval->max[1].dist = root_2[1];
+				}
+				else if (root_1[1] < root_2[1])
+				{
+					interval->max[0].dist = root_2[0];
+					interval->min[1].dist = root_1[1];
+					interval->max[1].dist = root_2[1];
+				}
+				else
+				{
+					interval->max[0].dist = root_2[0];
+					interval->min[1].dist = root_2[1];
+					interval->max[1].dist = root_1[1];
+				}
+			}
+			else
+			{
+				interval->min[0].dist = root_2[0];
+				if (root_2[1] < root_1[0])
+				{
+					interval->max[0].dist = root_2[1];
+					interval->min[1].dist = root_1[0];
+					interval->max[1].dist = root_1[1];
+				}
+				else if (root_2[1] < root_1[1])
+				{
+					interval->max[0].dist = root_1[0];
+					interval->min[1].dist = root_2[1];
+					interval->max[1].dist = root_1[1];
+				}
+				else
+				{
+					interval->max[0].dist = root_1[0];
+					interval->min[1].dist = root_1[1];
+					interval->max[1].dist = root_2[1];
+				}
+			}
+		}
+		else
+		{
+			interval->nb_hit = 1;
+			interval->min[0].dist = root_1[0];
+			interval->max[0].dist = root_1[1];
+		}
+	}
+	else if (quad2_solve(r_g[1], r_h[1], root_2))
+	{
+		interval->nb_hit = 1;
+		interval->min[0].dist = root_2[0];
+		interval->max[0].dist = root_2[1];
+	}
+	return (interval->nb_hit);
+	/*	//printf("not 2\n");
 	//printf("x1 %f x2 %f x3 %f x4 %f\n", root_1[0], root_1[1], root_2[0], root_2[1]);
 
 	double	root;
@@ -158,19 +198,20 @@ int		quad4_solve(double a, double b, double c, double d, t_interval *interval)
 		return (0);
 	//printf("x^4+ %fx^3 + %fx^2 +%fx + %f\n",a,b,c,d);
 	//printf("x1 %f x2 %f x3 %f x4 %f\n" ,root_1[0], root_1[1], root_2[0], root_2[1]);
-	interval->min[0].dist = root;
-	interval->max[0].dist = root;
+	interval->min[0].dist.dist = root;
+	interval->max[0].dist.dist = root;
 	if (root > 0)
 	{
-		interval->min[0].normal = OUTWARDS;
-		interval->max[0].normal = INWARDS;
+		interval->min[0].dist.normal = OUTWARDS;
+		interval->max[0].dist.normal = INWARDS;
 	}
 	else
 	{
-		interval->min[0].normal = INWARDS;
-		interval->max[0].normal = OUTWARDS;
+		interval->min[0].dist.normal = INWARDS;
+		interval->max[0].dist.normal = OUTWARDS;
 	}
 	return (1);
+	*/
 }
 /*
 int			quad4_solve(double a, double b, double c, double d, double e, t_interval *interval)
@@ -269,17 +310,17 @@ int			quad4_solve(double a, double b, double c, double d, double e, t_interval *
 		root = x3;
 	if ( x4 > 0 && x4 < x1 && x4 < x2 && x4 < x3 )
 		root = x4;
-	interval->min[0].dist = root;
-	interval->max[0].dist = root;
+	interval->min[0].dist.dist = root;
+	interval->max[0].dist.dist = root;
 	if (root > 0)
 	{
-		interval->min[0].normal = OUTWARDS;
-		interval->max[0].normal = INWARDS;
+		interval->min[0].dist.normal = OUTWARDS;
+		interval->max[0].dist.normal = INWARDS;
 	}
 	else
 	{
-		interval->min[0].normal = INWARDS;
-		interval->max[0].normal = OUTWARDS;
+		interval->min[0].dist.normal = INWARDS;
+		interval->max[0].dist.normal = OUTWARDS;
 	}
 	return (1);
 }
--- srcs/renderer/objects/torus.c
+++ srcs/renderer/objects/torus.c
@@ -6,7 +6,7 @@
 /*   By: racousin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/14 10:32:05 by racousin          #+#    #+#             */
-/*   Updated: 2017/03/17 16:29:24 by racousin         ###   ########.fr       */
+/*   Updated: 2017/03/18 19:49:17 by racousin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ int				torus_intersect(t_obj *self, t_ray *ray, t_interval *interval)
 
 	//self->radius = 1;
 	self->length = 0.2;
-	//self->dir.x = 0;
-	//self->dir.y = 0;
-	//self->dir.z = 1;
+	//self->dir.x = EPS;
+	//self->dir.y = EPS;
+	//self->dir.z = EPS;
 	//self->kdiff = 1;
 	//self->kspec = 1;
 	ray->dir = vec3_get_normalized(ray->dir);
@@ -42,21 +42,23 @@ int				torus_intersect(t_obj *self, t_ray *ray, t_interval *interval)
 	double	u = vec3_dot(self->dir, Q);
 	double	v = vec3_dot(self->dir, ray->dir);
 
-	double	a = 1 - pow(v, 2);
-	double	b = 2 * (vec3_dot(Q, ray->dir) - u * v);
-	double	c = vec3_dot(Q, Q) - pow(u, 2);
-	double	d = vec3_dot(Q, Q) + pow(R, 2) - pow(r, 2);
+	double	a = 1. - pow(v, 2.);
+	double	b = 2. * (vec3_dot(Q, ray->dir) - u * v);
+	double	c = vec3_dot(Q, Q) - pow(u, 2.);
+	double	d = vec3_dot(Q, Q) + pow(R, 2.) - pow(r, 2.);
 
-	double	A = 1;
-	double	B = 4 * vec3_dot(Q, ray->dir);
-	double	C = 2 * d + pow(B, 2) / 4. - 4 * pow(R, 2) * a;
-	double	D = B * d - 4 * pow(R, 2) * b;
-	double	E = pow(d, 2) - 4 * pow(R, 2) * c;
+	double	A = 1.;
+	double	B = 4. * vec3_dot(Q, ray->dir);
+	double	C = 2. * d + pow(B, 2.) / 4. - 4. * pow(R, 2.) * a;
+	double	D = B * d - 4. * pow(R, 2.) * b;
+	double	E = pow(d, 2.) - 4. * pow(R, 2.) * c + EPS;
 
 	if ((interval->nb_hit = quad4_solve(B,C,D,E, interval)))
 	{
 		interval->min[0].ref = *self;
 		interval->max[0].ref = *self;
+		interval->min[1].ref = *self;
+		interval->max[1].ref = *self;
 		return (1);
 	}
 	return (0);
@@ -76,5 +78,8 @@ t_vec3			torus_normal(t_obj *self, t_vec3 pos)
 	y = vec3_dot(vec3_sub(pos, self->pos), self->dir);
 	D = vec3_sub(vec3_sub(pos, self->pos), vec3_mult(y, self->dir));
 	X = vec3_mult(self->radius / sqrt(vec3_dot(D, D)), D);
-	return (vec3_get_normalized(vec3_sub(pos, vec3_add(self->pos ,X))));
+	if (self->normal_dir == OUTWARDS)
+		return (vec3_get_normalized(vec3_sub(pos, vec3_add(self->pos ,X))));
+	else
+		return (vec3_get_normalized(vec3_sub(vec3_add(self->pos ,X), pos)));
 }
