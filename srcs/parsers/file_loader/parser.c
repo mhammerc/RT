aa77ffb06926021fd976161d11bcd06028ec4f32
Martin Hammerchmidt <martin.hammerchmidt@gmail.com>
2017-03-21 15:42:18 +0100

    Fixed infinite loop with /dev/random

--- srcs/parsers/file_loader/parser.c
+++ srcs/parsers/file_loader/parser.c
@@ -1,11 +1,14 @@
 #include "ui.h"
 
-static void		ft_parse_line(t_env *env)
+static int		ft_parse_line(t_env *env)
 {
 	if (ft_strncmp(env->ln, "camera:", 7) == 0)
 		p_parse_camera(env);
 	else if (ft_strncmp(env->ln, "object:", 7) == 0)
 		p_parse_object(env);
+	else
+		return (0);
+	return (1);
 }
 
 void			ft_read_file(char *name, t_env *env)
@@ -28,7 +31,8 @@ void			ft_read_file(char *name, t_env *env)
 		if (ret == -1)
 			ft_puterr(ERR_FILE_OPEN, env);
 		if (ft_strcmp(env->ln, "") != 0 && *env->ln != '#')
-			ft_parse_line(env);
+			if (!ft_parse_line(env))
+				return ;
 	}
 	free(env->ln);
 }
