b5146c2c4e6e1ac58259403c056a8caea0460ca6
Gaetan POBLON <gpoblon@student.42.fr>
2017-03-23 16:36:55 +0100

    added some scenes

--- srcs/ui/right_panel/obj_situational_update.c
+++ srcs/ui/right_panel/obj_situational_update.c
@@ -6,7 +6,7 @@
 /*   By: gpoblon <gpoblon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/22 20:32:45 by gpoblon           #+#    #+#             */
-/*   Updated: 2017/03/23 11:14:25 by gpoblon          ###   ########.fr       */
+/*   Updated: 2017/03/23 16:35:06 by gpoblon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ void			add_situational_widgets(t_ui *ui, t_object *focused_obj,
 	add_situational_widgets_aux(ui, focused_obj, props, type);
 	if (type != CSG)
 		create_color_chooser(ui, focused_obj->color);
-	if (type != CSG || type != EMPTY || type != LIGHT)
+	if (type != CSG && type != EMPTY && type != LIGHT)
 	{
 		add_material_widgets_aux(ui, focused_obj, props);
 		add_material_widgets(ui, focused_obj, props);
