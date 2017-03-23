8227d6423e90e74d93d2118a11d7ca6550b98f35
Audran DITSCH <aditsch@e1r4p2.42.fr>
2017-03-23 18:13:07 +0100

    fix

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: gpoblon <gpoblon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/22 12:05:41 by gpoblon           #+#    #+#             */
-/*   Updated: 2017/03/22 18:19:21 by gpoblon          ###   ########.fr       */
+/*   Updated: 2017/03/23 18:12:10 by aditsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@ int			main(int argc, char **argv)
 	static GtkApplication	*app;
 	int						status;
 
+    (void)argc;
+    (void)argv;
 	app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
 	g_signal_connect(app, "activate", G_CALLBACK(init_interface), NULL);
-	status = g_application_run(G_APPLICATION(app), argc, argv);
+	status = g_application_run(G_APPLICATION(app), 0, NULL);
 	g_object_unref(app);
 	return (status);
 }
